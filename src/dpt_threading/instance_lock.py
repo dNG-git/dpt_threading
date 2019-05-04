6791da5146370cedbc32269afd0eb182a409e06c
NotTheEvilOne <git-NTEO@vplace.de>
2019-05-04 12:36:42 +0200

    Initial commit of "pas_core" splitted Python module

---
+++ src/dpt_threading/instance_lock.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+"""
+direct Python Toolbox
+All-in-one toolbox to encapsulate Python runtime variants
+----------------------------------------------------------------------------
+(C) direct Netware Group - All rights reserved
+https://www.direct-netware.de/redirect?dpt;threading
+
+This Source Code Form is subject to the terms of the Mozilla Public License,
+v. 2.0. If a copy of the MPL was not distributed with this file, You can
+obtain one at http://mozilla.org/MPL/2.0/.
+----------------------------------------------------------------------------
+https://www.direct-netware.de/redirect?licenses;mpl2
+----------------------------------------------------------------------------
+#echo(dptThreadingVersion)#
+#echo(__FILEPATH__)#
+"""
+
+from dpt_settings import Settings
+
+from .thread_lock import ThreadLock
+
+class InstanceLock(ThreadLock):
+    """
+"InstanceLock" is used to protect manipulation of a singleton.
+
+:author:     direct Netware Group et al.
+:copyright:  direct Netware Group - All rights reserved
+:package:    dpt
+:subpackage: threading
+:since:      v1.0.0
+:license:    https://www.direct-netware.de/redirect?licenses;mpl2
+             Mozilla Public License, v. 2.0
+    """
+
+    def __init__(self, timeout = None):
+        """
+Constructor __init__(InstanceLock)
+
+:since: v1.0.0
+        """
+
+        ThreadLock.__init__(self)
+
+        self._timeout = (Settings.get("pas_global_singleton_lock_timeout", 3) if (timeout is None) else timeout)
+    #
+#
